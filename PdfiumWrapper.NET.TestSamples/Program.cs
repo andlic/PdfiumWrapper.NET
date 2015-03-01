f155ae141c8c1760b2386116e852644b24322c5a
Pavel <finita18@gmail.com>
2015-03-01 16:14:51 +0300

    Added PageSet class

--- PdfiumWrapper.NET.TestSamples/Program.cs
+++ PdfiumWrapper.NET.TestSamples/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Windows.Media.Imaging;
 
 namespace PdfiumWrapper.NET.TestSamples
@@ -12,7 +13,7 @@ namespace PdfiumWrapper.NET.TestSamples
             int width;
             int height;
 
-            using (var pdfDoc = new PdfDocument(@"test.pdf"))
+            using (var pdfDoc = new PdfDocument(@"d:\2.pdf"))
             {
                 int pageCount = pdfDoc.PageCount;
                 for (int i = 0; i < pageCount; ++i)
@@ -25,6 +26,20 @@ namespace PdfiumWrapper.NET.TestSamples
             }
 
             PdfiumDLL.Destroy();
+
+            var set = new PageSet();
+            set.Add(42);
+            set.Add(23);
+            set.Add(new PageInterval(16, 20));
+            set.Add(new PageInterval(50, 70));
+
+            foreach (var index in set)
+                Console.Write(index.ToString() + "  ");
+
+            set.Add(new PageInterval(52, 73));
+            set.Add(new PageInterval(40, 50));
+            set.Add(new PageInterval(10, 80));
+            set.Add(new PageInterval(11, 70));
         }
 
 
