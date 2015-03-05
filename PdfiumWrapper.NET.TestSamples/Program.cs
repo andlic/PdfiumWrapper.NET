6665dc7374cd563e055ac135fe8a9a9616b12d95
Pavel <finita18@gmail.com>
2015-03-05 12:25:51 +0300

    Revert last commit

--- PdfiumWrapper.NET.TestSamples/Program.cs
+++ PdfiumWrapper.NET.TestSamples/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Windows.Media.Imaging;
 
 namespace PdfiumWrapper.NET.TestSamples
@@ -13,7 +12,7 @@ namespace PdfiumWrapper.NET.TestSamples
             int width;
             int height;
 
-            using (var pdfDoc = new PdfDocument(@"d:\2.pdf"))
+            using (var pdfDoc = new PdfDocument(@"test.pdf"))
             {
                 int pageCount = pdfDoc.PageCount;
                 for (int i = 0; i < pageCount; ++i)
@@ -26,20 +25,6 @@ namespace PdfiumWrapper.NET.TestSamples
             }
 
             PdfiumDLL.Destroy();
-
-            var set = new PageSet();
-            set.Add(42);
-            set.Add(23);
-            set.Add(new PageInterval(16, 20));
-            set.Add(new PageInterval(50, 70));
-
-            foreach (var index in set)
-                Console.Write(index.ToString() + "  ");
-
-            set.Add(new PageInterval(52, 73));
-            set.Add(new PageInterval(40, 50));
-            set.Add(new PageInterval(10, 80));
-            set.Add(new PageInterval(11, 70));
         }
 
 
